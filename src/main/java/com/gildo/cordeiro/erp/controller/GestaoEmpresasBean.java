8d66fa9ad18a7d253994eeab304f74eec9a82798
gildo-cordeiro <gildo.duartec@gmail.com>
2021-02-26 23:23:45 -0300

    Aula 4.1 - Listando empresas com tabela de dados

--- src/main/java/com/gildo/cordeiro/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/gildo/cordeiro/erp/controller/GestaoEmpresasBean.java
@@ -1,35 +1,31 @@
 package com.gildo.cordeiro.erp.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.gildo.cordeiro.erp.model.Empresa;
-import br.com.gildo.cordeiro.erp.model.TipoEmpresa;
+import br.com.gildo.cordeiro.erp.repository.Empresas;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
     
-    private static final long serialVersionUID = 1L;
-    
-   private Empresa empresa = new Empresa();
+   private static final long serialVersionUID = 1L;
    
-   public void salvar() {
-	   System.out.println("Razao: "+ empresa.getRazaoSocial() + " - Nome fantasia: " + empresa.getNomeFantasia() + " - Tipo: "+ empresa.getTipo());
-   }
+   @Inject
+   private Empresas empresas;
    
-   public Empresa getEmpresa() {
-	   return empresa;
-   }
+   private List<Empresa> listEmpresas;
    
-   public TipoEmpresa[] getTiposEmpresa() {
-	   return TipoEmpresa.values();
-   }
-   
-   public String ajuda() {
-	   return "AjudaGestaoEmpresas";
+   public void todasEmpresas() {
+	   listEmpresas = empresas.todas();
+   }   
+   public List<Empresa> getListEmpresas() {
+	   return listEmpresas;
    }
    
 }
